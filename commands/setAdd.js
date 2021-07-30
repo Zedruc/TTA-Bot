ada49fc6aa0fd5352e98550f51d823ef853ab5eb
Nofu <69540929+Zedruc@users.noreply.github.com>
2021-07-30 17:20:21 +0200

    set-add command

---
+++ commands/setAdd.js
@@ -0,0 +1,75 @@
+const { MessageCollector } = require('discord.js-collector');
+const levelModel = require('../models/levelSchema');
+
+module.exports = {
+    name: "set-add",
+    description: "Allows judges to add a level with provided data",
+    async execute(message, args, client, Discord, cmd, profileData) {
+        if (!(message.member.roles.cache.some(r => r.name === "Judge" || r.name === "Moderator" || r.name === "Administrator" || r.name === "OwOner"))) {
+            var embed = new Discord.MessageEmbed()
+                .setDescription("You have to be a staff member to set a user's points!")
+                .setColor("#ff0000");
+            return message.channel.send(embed);
+        }
+
+        var botMessage = await message.channel.send("What's the level ID?");
+        var userMessage = await MessageCollector.asyncQuestion({
+            botMessage,
+            user: message.author.id
+        });
+
+        if (!(userMessage.content.length === 11)) {
+            return message.channel.send("The level ID must consist of 11 symbols! LEV-ELC-ODE");
+        }
+
+        var data = {
+            levelID: Number(),
+            levelName: String(),
+            creator: String()
+        }
+
+        data.levelID = userMessage.content;
+        userMessage.delete();
+
+        botMessage.edit("What's the level's name?");
+        var userMessage = await MessageCollector.asyncQuestion({
+            botMessage,
+            user: message.author.id
+        });
+        data.levelName = userMessage.content;
+        userMessage.delete();
+
+        botMessage.edit("Who is the level's creator? (Ingame Name)");
+        var userMessage = await MessageCollector.asyncQuestion({
+            botMessage,
+            user: message.author.id
+        });
+        data.creator = userMessage.content;
+        userMessage.delete();
+
+        var levelExists = await levelModel.findOne({ levelID: data.levelID });
+        if (levelExists) {
+            botMessage.delete();
+            return message.channel.send(`A level with the ID \`${levelExists.levelID}\` was already registered by ${levelExists.creator}!`);
+        }
+
+        botMessage.edit("Setting everything up...");
+
+        var level = await levelModel.create({
+            levelID: data.levelID.toLowerCase(),
+            levelName: data.levelName,
+            creator: data.creator
+        });
+
+        level.save();
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Succesfully added level")
+            .addField("ID", data.levelID)
+            .addField("Name", data.levelName)
+            .addField("Creator", data.creator)
+            .setColor("#4BB543");
+
+        message.channel.send(embed);
+    }
+}
