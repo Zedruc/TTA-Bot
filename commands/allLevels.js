7a8e1d019c74b0ac6ee8d86e04c5a3910d8d57d3
Nofu <69540929+Zedruc@users.noreply.github.com>
2021-09-25 11:38:57 +0200

    List command

---
+++ commands/allLevels.js
@@ -0,0 +1,80 @@
+const levelModel = require('../models/levelSchema');
+const FILTERS = ['tag', 'creator', 'difficulty'];
+
+module.exports = {
+    name: "list",
+    description: "Show a list of all specified levels",
+    async execute(message, args, client, Discord, cmd, profileData, prefix) {
+        if (!(message.member.roles.cache.some(r => r.name === "Judge" || r.name === "Moderator" || r.name === "Administrator" || r.name === "OwOner"))) {
+            var embed = new Discord.MessageEmbed()
+                .setDescription("You have to be a staff member to view the level list!")
+                .setColor("#ff0000");
+            return message.channel.send(embed);
+        }
+
+        if (args.length == 0) {
+            createList({});
+        }
+        if (args[0]) {
+            if (!(FILTERS.includes(args[0]))) {
+                var embed = new Discord.MessageEmbed()
+                    .setDescription(`${args[0]} isn't a valid filter.`)
+                    .setColor("#ff0000");
+                return message.channel.send(embed);
+            }
+
+            const filterArg = message.content.slice(prefix.length + 6 + args[0].length, message.content.length);
+
+            switch (args[0]) {
+                case 'tag':
+                    createList({ 'tags': filterArg });
+                    break;
+                case 'creator':
+                    createList({ 'creator': filterArg });
+                    break;
+                case 'difficulty':
+                    createList({ 'difficulty': Number(filterArg) });
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        function createList(filter) {
+            levelModel.find(filter, (err, docs) => {
+                if (err) {
+                    console.error(err);
+                    var embed = new Discord.MessageEmbed()
+                        .setDescription("You have to be a staff member to delete a user's level!")
+                        .setColor("#ff0000");
+                    return message.channel.send(embed);
+                }
+
+                if (docs.length < 1) {
+                    var embed = new Discord.MessageEmbed()
+                        .setDescription("No levels matching your filter found.")
+                        .setColor("#ff0000");
+                    return message.channel.send(embed);
+                }
+
+                var selfText = "";
+                var thisLevelsVideos = "";
+                var embed = new Discord.MessageEmbed()
+                    .setColor("#4BB543");
+                docs.forEach((level) => {
+                    if (selfText.length > 3300) {
+                        embed.setDescription(selfText);
+                        message.channel.send(embed);
+                        selfText = "";
+                    }
+                    if (level.clearVideos.length) {
+                        level.clearVideos.forEach(videoLink = thisLevelsVideos += `${videoLink}\n`);
+                    }
+                    selfText += `**"${level.levelName}"** by **${level.creator}**\nApproved: ${level.approved} (${level.difficulty})\nID: ${level.levelID}\nLikes/Dislikes: ${level.likes}/${level.dislikes}\nClear videos: ${(thisLevelsVideos.length > 0) ? thisLevelsVideos : "None"}\n======================\n\n`;
+                });
+                embed.setDescription(selfText);
+                message.channel.send(embed);
+            });
+        }
+    }
+}
